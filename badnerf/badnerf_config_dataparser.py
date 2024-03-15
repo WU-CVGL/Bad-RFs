14f0bfbb54be285eda32e534a851db1008e7a15b
Lingzhe Zhao <43722188+LingzheZhao@users.noreply.github.com>
2024-03-15 22:36:46 +0800

    Add Deblur-NeRF data parser; Update doc on data & training (#9)
    * add DeblurNerfDataParser
    
    * update README
    
    * minor

--- badnerf/badnerf_config_dataparser.py
+++ badnerf/badnerf_config_dataparser.py
@@ -5,6 +5,7 @@ BAD-NeRF dataparser config.
 from nerfstudio.plugins.registry_dataparser import DataParserSpecification
 
 from badnerf.image_restoration_dataparser import ImageRestorationDataParserConfig
+from badnerf.deblur_nerf_dataparser import DeblurNerfDataParserConfig
 
-
-BadNerfDataparser = DataParserSpecification(config=ImageRestorationDataParserConfig())
+ImageRestoreDataParser = DataParserSpecification(config=ImageRestorationDataParserConfig())
+DeblurNerfDataParser = DataParserSpecification(config=DeblurNerfDataParserConfig())
